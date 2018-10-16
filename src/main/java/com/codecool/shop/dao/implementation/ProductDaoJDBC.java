9a4028982dfe0acaffd8172e1e2ccce0b940213a
Toth <toth.balazs.roland@gmail.com>
2018-10-16 12:42:28 +0200

    database structure changes, products can be added to database

---
+++ src/main/java/com/codecool/shop/dao/implementation/ProductDaoJDBC.java
@@ -0,0 +1,68 @@
+package com.codecool.shop.dao.implementation;
+
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.model.Product;
+import com.codecool.shop.model.ProductCategory;
+import com.codecool.shop.model.Supplier;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
+public class ProductDaoJDBC implements ProductDao {
+
+    private static final String DATABASE = "jdbc:postgresql://localhost:5432/shoptest";
+    private static final String DB_USER = "postgres";
+    private static final String DB_PASSWORD = "q8h1j5b321";
+
+    @Override
+    public void add(Product product) {
+        try {
+            Connection connection = getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO products (category_id,supplier_id,price,currency,name,description) VALUES (?,?,?,?,?,?);");
+            preparedStatement.setInt(1,product.getProductCategory().getId());
+            preparedStatement.setInt(2,product.getSupplier().getId());
+            preparedStatement.setDouble(3,product.getDefaultPrice());
+            preparedStatement.setString(4,product.getDefaultCurrency().toString());
+            preparedStatement.setString(5,product.getName());
+            preparedStatement.setString(6,product.getDescription());
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public Product find(int id) {
+        return null;
+    }
+
+    @Override
+    public void remove(int id) {
+
+    }
+
+    @Override
+    public List<Product> getAll() {
+        return null;
+    }
+
+    @Override
+    public List<Product> getBy(Supplier supplier) {
+        return null;
+    }
+
+    @Override
+    public List<Product> getBy(ProductCategory productCategory) {
+        return null;
+    }
+
+    private Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(
+                DATABASE,
+                DB_USER,
+                DB_PASSWORD);
+    }
+}
--- src/main/sql/init_db.sql
+++ src/main/sql/init_db.sql
@@ -1,26 +1,26 @@
-ALTER TABLE "Order" DROP CONSTRAINT IF EXISTS "Order_fk0";
+ALTER TABLE "order" DROP CONSTRAINT IF EXISTS "Order_fk0";
 
-ALTER TABLE "LineItems" DROP CONSTRAINT IF EXISTS "LineItems_fk0";
+ALTER TABLE lineItems DROP CONSTRAINT IF EXISTS "LineItems_fk0";
 
-ALTER TABLE "LineItems" DROP CONSTRAINT IF EXISTS "LineItems_fk1";
+ALTER TABLE lineItems DROP CONSTRAINT IF EXISTS "LineItems_fk1";
 
-ALTER TABLE "Products" DROP CONSTRAINT IF EXISTS "Products_fk0";
+ALTER TABLE products DROP CONSTRAINT IF EXISTS "Products_fk0";
 
-ALTER TABLE "Products" DROP CONSTRAINT IF EXISTS "Products_fk1";
+ALTER TABLE products DROP CONSTRAINT IF EXISTS "Products_fk1";
 
-DROP TABLE IF EXISTS "Order";
+DROP TABLE IF EXISTS "order";
 
-DROP TABLE IF EXISTS "Users";
+DROP TABLE IF EXISTS users;
 
-DROP TABLE IF EXISTS "LineItems";
+DROP TABLE IF EXISTS lineitems;
 
-DROP TABLE IF EXISTS "Products";
+DROP TABLE IF EXISTS products;
 
-DROP TABLE IF EXISTS "Categories";
+DROP TABLE IF EXISTS categories;
 
-DROP TABLE IF EXISTS "Suppliers";
+DROP TABLE IF EXISTS suppliers;
 
-CREATE TABLE "Order" (
+CREATE TABLE "order" (
 	"id" serial NOT NULL,
 	"user_id" integer NOT NULL,
 	"total_price" FLOAT,
@@ -33,7 +33,7 @@ CREATE TABLE "Order" (
 
 
 
-CREATE TABLE "Users" (
+CREATE TABLE "users" (
 	"id" serial NOT NULL,
 	"password" VARCHAR(255) NOT NULL,
 	"first_name" VARCHAR(255) NOT NULL,
@@ -49,7 +49,7 @@ CREATE TABLE "Users" (
 
 
 
-CREATE TABLE "LineItems" (
+CREATE TABLE "lineitems" (
 	"order_id" integer NOT NULL,
 	"quantity" integer NOT NULL,
 	"product_id" integer NOT NULL
@@ -59,12 +59,14 @@ CREATE TABLE "LineItems" (
 
 
 
-CREATE TABLE "Products" (
+CREATE TABLE "products" (
 	"id" serial NOT NULL,
+	"name" VARCHAR(255) NOT NULL,
 	"category_id" integer NOT NULL,
 	"supplier_id" integer NOT NULL,
 	"price" FLOAT NOT NULL,
 	"currency" VARCHAR(255) NOT NULL,
+  "description" varchar(255) NOT NULL,
 	CONSTRAINT Products_pk PRIMARY KEY ("id")
 ) WITH (
   OIDS=FALSE
@@ -72,7 +74,7 @@ CREATE TABLE "Products" (
 
 
 
-CREATE TABLE "Categories" (
+CREATE TABLE "categories" (
 	"id" serial NOT NULL,
 	"name" VARCHAR(255) NOT NULL,
 	"description" VARCHAR(255) NOT NULL,
@@ -84,7 +86,7 @@ CREATE TABLE "Categories" (
 
 
 
-CREATE TABLE "Suppliers" (
+CREATE TABLE "suppliers" (
 	"id" serial NOT NULL,
 	"name" VARCHAR(255) NOT NULL,
 	"description" VARCHAR(255) NOT NULL,
@@ -95,14 +97,14 @@ CREATE TABLE "Suppliers" (
 
 
 
-ALTER TABLE "Order" ADD CONSTRAINT "Order_fk0" FOREIGN KEY ("user_id") REFERENCES "Users"("id");
+ALTER TABLE "order" ADD CONSTRAINT "Order_fk0" FOREIGN KEY ("user_id") REFERENCES users("id");
 
 
-ALTER TABLE "LineItems" ADD CONSTRAINT "LineItems_fk0" FOREIGN KEY ("order_id") REFERENCES "Order"("id");
-ALTER TABLE "LineItems" ADD CONSTRAINT "LineItems_fk1" FOREIGN KEY ("product_id") REFERENCES "Products"("id");
+ALTER TABLE lineitems ADD CONSTRAINT "LineItems_fk0" FOREIGN KEY ("order_id") REFERENCES "order"("id");
+ALTER TABLE lineitems ADD CONSTRAINT "LineItems_fk1" FOREIGN KEY ("product_id") REFERENCES products("id");
 
-ALTER TABLE "Products" ADD CONSTRAINT "Products_fk0" FOREIGN KEY ("category_id") REFERENCES "Categories"("id");
-ALTER TABLE "Products" ADD CONSTRAINT "Products_fk1" FOREIGN KEY ("supplier_id") REFERENCES "Suppliers"("id");
+ALTER TABLE products ADD CONSTRAINT "Products_fk0" FOREIGN KEY ("category_id") REFERENCES categories("id");
+ALTER TABLE products ADD CONSTRAINT "Products_fk1" FOREIGN KEY ("supplier_id") REFERENCES suppliers("id");
 
 
 
