697597e8453710a4d02731a0d804f0c1291c214f
Toth <toth.balazs.roland@gmail.com>
2018-11-14 09:55:57 +0100

    logger implemented

--- src/main/java/com/codecool/shop/controller/ChangeQuantityController.java
+++ src/main/java/com/codecool/shop/controller/ChangeQuantityController.java
@@ -2,6 +2,8 @@ package com.codecool.shop.controller;
 
 import com.codecool.shop.dao.implementation.CurrentOrders;
 import com.codecool.shop.model.Order;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,15 +14,16 @@ import java.io.IOException;
 
 @WebServlet(urlPatterns = {"/ajax/change-quantity"})
 public class ChangeQuantityController extends HttpServlet {
+    private static final Logger logger = LoggerFactory.getLogger(ChangeQuantityController.class);
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String productname = req.getParameter("productName");
         int newQuantity = Integer.parseInt(req.getParameter("quantity"));
-        System.out.println(newQuantity);
-        System.out.println(productname);
         Order currentOrder = CurrentOrders.getOrder(1);
         currentOrder.modifyQuantity(productname,newQuantity);
+        logger.info("New quantity of {} set to {}.",productname,newQuantity);
+
 
         resp.setContentType("text/plain");  // Set content type of the response so that jQuery knows what it can expect.
         resp.setCharacterEncoding("UTF-8"); // You want world domination, huh?
